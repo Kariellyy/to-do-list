efc9970bdef05c4d9ef83fa7553732d5932ba519
Karielly de Carvalho <kariellyycarvalho@gmail.com>
2024-04-05 14:51:26 -0300

    Atualizações de idioma e estilos de CSS

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,21 +1,22 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => { // função para carregar o conteúdo da página
     const inputTarefa = document.getElementById('nome-tarefa');
     const inputDescricao = document.getElementById('descricao');
     const inputEtiqueta = document.getElementById('etiqueta');
     const inputData = document.getElementById('data');
     const botaoSalvar = document.querySelector('.salvar');
+    const inputRepositorio = document.getElementById('task-repo');
     const listaTarefas = document.querySelector('.tarefas');
 
-    let tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
-    let indiceEdicao = null;
+    let tarefas = JSON.parse(localStorage.getItem('tarefas')) || []; // variável para armazenar tarefas
+    let indiceEdicao = null; // utilizado armazenar o índice da tarefa que está sendo editada
 
-    const salvarTarefas = () => {
+    const salvarTarefas = () => { // função para salvar tarefas no localStorage
         localStorage.setItem('tarefas', JSON.stringify(tarefas));
     };
 
-    init();
+    init(); // Inicio a aplicação
 
-    async function init() {
+    async function init() { // Função para buscar repositórios do GitHub
 
         let userName = localStorage.getItem('userName');
         if (userName === null || userName === '') {
@@ -73,18 +74,32 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    const renderizarTarefas = () => {
+    const funcionalidadesTarefas = () => { // função para exibir funcionalidades na tarefa
         listaTarefas.innerHTML = '<h2>Lista de Tarefas</h2>';
         tarefas.forEach((tarefa, index) => {
+            const dataFormatada = new Date(tarefa.data).toLocaleDateString('pt-BR');
+            let nomeRepositorio = '';
+            if (tarefa.github) {
+                const url = new URL(tarefa.github);
+                nomeRepositorio = url.pathname.split('/').pop();
+            }
             const divTarefa = document.createElement('div');
             divTarefa.className = `tarefa ${tarefa.concluido ? 'concluida' : ''}`;
             divTarefa.innerHTML = `
                 <div class="tarefa-info">
-                    <h3>${tarefa.nome}</h3>
+                    <h4>${tarefa.nome}</h4>
                     <p>${tarefa.descricao}</p>
                     <div class="etiqueta-data">
-                        <ion-icon class="etiqueta" name="pricetag-outline"></ion-icon> ${tarefa.etiqueta}
-                        <ion-icon class="data" name="calendar-outline"></ion-icon> ${tarefa.data}
+                        <span class="etiqueta">
+                        <ion-icon name="pricetag-outline"></ion-icon>
+                        ${tarefa.etiqueta}</span>
+                        <span class="data">
+                            <ion-icon name="calendar-outline"></ion-icon>
+                        ${dataFormatada}
+                        </span>
+                        <span class="github">
+                            <a href="${tarefa.github}" target="_blank"><ion-icon name="logo-github"></ion-icon> ${nomeRepositorio}</a>
+                        </span>
                     </div>
                 </div>
                 <div class="tarefa-actions">
@@ -97,13 +112,14 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     };
 
-    botaoSalvar.addEventListener('click', () => {
+    botaoSalvar.addEventListener('click', () => { // função para salvar tarefas
         const tarefaData = {
             nome: inputTarefa.value,
             descricao: inputDescricao.value,
             etiqueta: inputEtiqueta.value,
             data: inputData.value,
-            concluido: indiceEdicao !== null ? tarefas[indiceEdicao].concluido : false
+            concluido: indiceEdicao !== null ? tarefas[indiceEdicao].concluido : false,
+            github: inputRepositorio.value
         };
 
         if (indiceEdicao !== null) {
@@ -115,7 +131,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
         salvarTarefas();
-        renderizarTarefas();
+        funcionalidadesTarefas();
 
         inputTarefa.value = '';
         inputDescricao.value = '';
@@ -124,7 +140,7 @@ document.addEventListener('DOMContentLoaded', () => {
         indiceEdicao = null;
     });
 
-    listaTarefas.addEventListener('click', (event) => {
+    listaTarefas.addEventListener('click', (event) => { // função para editar, excluir e concluir tarefas
         if (event.target.closest('.editar')) {
             indiceEdicao = parseInt(event.target.closest('.editar').dataset.index);
             const tarefa = tarefas[indiceEdicao];
@@ -136,14 +152,14 @@ document.addEventListener('DOMContentLoaded', () => {
             const index = parseInt(event.target.closest('.excluir').dataset.index);
             tarefas.splice(index, 1);
             salvarTarefas();
-            renderizarTarefas();
+            funcionalidadesTarefas();
         } else if (event.target.closest('.concluir')) {
             const index = parseInt(event.target.closest('.concluir').dataset.index);
             tarefas[index].concluido = !tarefas[index].concluido;
             salvarTarefas();
-            renderizarTarefas();
+            funcionalidadesTarefas();
         }
     });
 
-    renderizarTarefas();
+    funcionalidadesTarefas();
 });
